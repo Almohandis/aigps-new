a98f5a43aaeb17c74c56bfe02c2233dbaad74449
Abdullah Hejazi <a.m.a.hejazi@gmail.com>
2022-01-30 08:07:25 +0200

    Added grey and red markers on map

--- resources/views/citizen/reservation2.blade.php
+++ resources/views/citizen/reservation2.blade.php
@@ -43,9 +43,29 @@
                 }
 
                 function initMap() {
+                    const blackMarker = {
+                        path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+                        fillColor: "black",
+                        fillOpacity: 0.7,
+                        strokeWeight: 0,
+                        rotation: 0,
+                        scale: 2,
+                        anchor: new google.maps.Point(15, 30),
+                    };
+
+                    const redMarker = {
+                        path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+                        fillColor: "red",
+                        fillOpacity: 0.7,
+                        strokeWeight: 0,
+                        rotation: 0,
+                        scale: 2,
+                        anchor: new google.maps.Point(15, 30),
+                    };
+
                     var locations = [
                         @foreach($campaigns as $campaign)
-                            ["{{ preg_replace('/\s+/', ' ', trim($campaign->address)); }}", {{ $campaign->location }}, {{$campaign->id}}],
+                            ["{{ preg_replace('/\s+/', ' ', trim($campaign->address)); }}", {{ $campaign->location }}, {{$campaign->id}}, "{{$campaign->start_date}}"],
                         @endforeach
                     ];
 
@@ -60,7 +80,8 @@
                     for (i = 0; i < locations.length; i++) {  
                         marker = new google.maps.Marker({
                             position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-                            map: map
+                            map: map,
+                            icon: new Date(locations[i][4]).getTime() > new Date().getTime() ? redMarker : blackMarker,
                         });
                         
                         google.maps.event.addListener(marker, 'click', (function(marker, i) {
