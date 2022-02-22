de18169589ef356be15c74323f96ef9b60fda694
Abdullah Hejazi <a.m.a.hejazi@gmail.com>
2022-02-22 08:56:34 +0200

    added article system

---
+++ resources/views/articles.blade.php
@@ -0,0 +1,20 @@
+<x-app-layout>
+    <div class="mt-6">
+        <h1 class="ml-5 text-left text-4xl text-white text-center" style="text-shadow: 2px 2px 8px #000000;">
+            List of articles
+        </h1>
+
+        @foreach($articles as $article)
+            <div class="mx-auto text-center mt-5 text-white">
+                @if($article->type == 'image')
+                    <img src="{{ $article->link }}" alt="">
+                @endif
+                <a href="/articles/{{ $article->id }}">
+                    {{ $article->title }}
+                </a>
+            </div>
+        @endforeach
+
+        
+    </div>
+</x-app-layout>
