913550caf273b3b6211a3ec10d731019acb0ac19
Bassel Saleh <bassellsaleh@hotmail.com>
2022-03-13 01:20:50 +0200

    appointments page

--- resources/views/citizen/appointments.blade.php
+++ resources/views/citizen/appointments.blade.php
@@ -1,14 +1,25 @@
 <x-app-layout>
     <div class="mt-6">
-        <h1 class="ml-5 text-left text-4xl text-white text-center" style="text-shadow: 2px 2px 8px #000000;">
+        <img src="{{ asset('appointments.jpg') }}" class="second-header">
+        <div class="divide"></div>
+        <div class="wrap"></div>
+        <h1 class="ml-5 text-left text-4xl text-white" style="text-shadow: 2px 2px 8px #000000;">
             My Appointments
         </h1>
+        <script>
+            function Scrolldown() {
+                        window.scroll(0,580); 
+                        
+                    
+                    }
+                    window.onload = Scrolldown;
+        </script>
 
         <!-- List appointments -->
         <div class="ml-5 mt-6">
             <div class="flex flex-wrap">
                 @foreach ($appointments as $appointment)
-                    <div class="bg-white shadow-md rounded-lg p-4">
+                    <div class="bg-white shadow-md rounded-lg p-4" id="appointments">
                         <div class="flex flex-wrap">
                             <div class="">
                                 <h3 class="text-left text-lg text-gray-800">
