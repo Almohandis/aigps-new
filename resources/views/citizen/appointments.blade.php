f22a4cd6b9398936829dec2e637793080288fe3e
Bassel Saleh <bassellsaleh@hotmail.com>
2022-03-13 02:54:35 +0200

    appointment list css

--- resources/views/citizen/appointments.blade.php
+++ resources/views/citizen/appointments.blade.php
@@ -17,26 +17,26 @@
 
         <!-- List appointments -->
         <div class="ml-5 mt-6">
-            <div class="flex flex-wrap">
+            <div class="flex flex-wrap" id="appointment_list">
                 @foreach ($appointments as $appointment)
                     <div class="bg-white shadow-md rounded-lg p-4" id="appointments">
                         <div class="flex flex-wrap">
                             <div class="">
-                                <h3 class="text-left text-lg text-gray-800">
+                                <h3 class="text-center text-lg text-gray-800">
                                     Appointment at: {{ $appointment->pivot->date }}
                                 </h3>
                             </div>
                         </div>
                         <div class="flex flex-wrap">
                             <div class="">
-                                <h3 class="text-left text-lg text-gray-800">
+                                <h3 class="text-center text-lg text-gray-800">
                                     {{ $appointment->address }}
                                 </h3>
                             </div>
                         </div>
                         <div class="flex flex-wrap">
-                            <div class="">
-                                <a href="/appointments/{{ $appointment->pivot->id }}/cancel" class="text-red-500">
+                            <div class="cancel-appointment">
+                                <a href="/appointments/{{ $appointment->pivot->id }}/cancel" class="text-red-500" id="cancel">
                                     Cancel
                                 </a>
                             </div>
