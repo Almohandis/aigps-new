43c720d892ff7b17832f8f40dcc7e696932672c6
Bassel Saleh <bassellsaleh@hotmail.com>
2022-03-15 23:08:49 +0200

    reservation form styling and adding back & cancel buttons

--- resources/views/citizen/reservation2.blade.php
+++ resources/views/citizen/reservation2.blade.php
@@ -31,7 +31,7 @@
         @endif
 
 
-        <div class="mx-auto text-center mt-5">
+        <div class="mx-auto text-center mt-5" style="background-color: #454040;">
 
             <form class="inline-block bg-black bg-opacity-50 p-8 text-justify" method="POST" action="/reserve/step2" id="reservation-form">
                 @if ($errors->any())
@@ -49,14 +49,14 @@
                 @endif
 
                 @csrf
-                <div>
+                <div id="address-div">
                     <x-label for="address" value="Adress" class="text-white" id="address-label"/>
 
                     <x-input id="address" class="block mt-1 w-full" type="text" name="address" :value="old('address')"
                         required autofocus />
                 </div>
 
-                <div>
+                <div class="country-div">
                     <x-label for="country" value="country" class="text-white" id="country-label"/>
 
                     <select name="country" id="country" class="block mt-1 w-full">
@@ -65,31 +65,32 @@
                         @endforeach
                     </select>
                 </div>
-
-                <div class="mt-3">
+                    <br><br>
+                <div class="mt-3" id="tele-div">
                     <x-label for="telephone_number" value="Telephone Number" class="text-white" id="tele-label" />
 
                     <x-input id="telephone_number" class="block mt-1 w-full" type="number" name="telephone_number"
                         :value="old('telephone_number')" required />
                 </div>
 
-                <div class="mt-3">
+                <div class="mt-3" id="birth-div">
                     <x-label for="birthdate" value="Birthdate" class="text-white" id="birth-label" />
 
                     <x-input id="birthdate" class="block mt-1 w-full" type="date" name="birthdate"
                         :value="old('birthdate')" required />
                 </div>
+                <br><br>
 
-                <div class="mt-3">
+                <div class="mt-3" id="gender-div">
                     <label for="gender" value="Gender" class="text-white font-medium text-sm" id="gender-label">Gender</label>
 
                     <input id="gender" class="ml-5" type="radio" name="gender" value="Male" />
-                    <label class="text-gray-400 text-sm mr-5">Male</label>
+                    <label class="text-gray-400 text-sm mr-5" id="gender-type">Male</label>
                     <input id="gender" type="radio" name="gender" value="Female" />
-                    <label class="text-gray-400 text-sm">Female</label>
+                    <label class="text-gray-400 text-sm" id="gender-type">Female</label>
                 </div>
-
-                <div class="mt-3">
+                    <br><br>
+                <div class="mt-3" id="mobile-div">
                     <x-label value="Mobile Numbers" class="text-white" id="mobile-label"/>
 
                     <div id="phones">
@@ -121,6 +122,10 @@
                             phone.setAttribute('placeholder', '+20');
                             phone.setAttribute('required', '');
                             phone.setAttribute('class', 'block mt-1');
+                            phone.style.height= "2.5rem";
+                            phone.style.borderRadius="5px";
+                            phone.style.marginLeft="10rem";
+                            phone.style.width="24rem";
 
                             phone_input.appendChild(phone);
 
@@ -143,9 +148,15 @@
                         }
                     </script>
                 </div>
+                <button onclick="window.location.href='/reserve'"class="back-btn">
+                    BACK
+                </button>
+                <button onclick="window.location.href='/'"class="cancel-btn">
+                    CANCEL
+                </button>
 
                 <div class="mt-6">
-                    <div class="mt-3 mx-auto text-right">
+                    <div class="mt-3 mx-auto text-right" id="next-btn">
                         <x-button>
                             Next
                         </x-button>
