b8a0bd0d755b4e38d364648d328d21af8529013f
Bassel Saleh <bassellsaleh@hotmail.com>
2022-03-17 02:45:00 +0200

    styling profile page

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -9,13 +9,13 @@
 
         <div class="mx-auto text-center mt-5">
 
-            <div class="inline-block bg-black bg-opacity-50 p-8 text-justify">
+            <div class="inline-block bg-black bg-opacity-50 p-8 text-justify" style="background-color: white;box-shadow: black;box-shadow: 0 .5rem 1.5rem rgba(0,0,0,.1);">
                 If you have a passport and want to request your <strong>medical passport</strong>, type in your
                 passport number and click on the button below
                 <form action="/medical-passport" method="POST">
                     @csrf
-                    <input type="text" name="passport_number">
-                    <input type="submit" value="Request medical passport">
+                    <input type="text" name="passport_number" style="margin-left: 15rem;margin-top: 2rem;">
+                    <input type="submit" value="Request medical passport" class="req-btn">
                 </form>
             </div>
         </div>
