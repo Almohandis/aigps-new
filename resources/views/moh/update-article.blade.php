a8e4b157872f4a9d54a0767b4ce781d86085f7b6
Abdullah Hejazi <a.m.a.hejazi@gmail.com>
2022-03-29 16:03:20 +0200

    update campaigns / articles

--- resources/views/moh/update-article.blade.php
+++ resources/views/moh/update-article.blade.php
@@ -1,60 +1,48 @@
 <x-app-layout>
-    <div class="max-w-6xl mx-auto sm:px-6 lg:px-8 mt-9">
-        <div class="notification" style="width: 18rem;">
-            @if (session('message'))
-            <img src="{{ asset('checkmark.png') }}" class="w-10 h-10 mx-auto" style="margin-left: 1rem;height: 25px;width: 25px;">
-               <p style="margin-top: -1.5rem;"> {{ session('message') }}</p>
-            @endif
-        </div>
+    <link href="{{asset('css/reservation.css')}}" rel="stylesheet">
 
-        @if ($errors->any())
-            <div>
-                <div class="font-medium text-red-600">
-                    {{ __('Whoops! Something went wrong.') }}
-                </div>
-
-                <ul class="mt-3 list-disc list-inside text-sm text-red-600">
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
+    <div class="mt-5 text-center">
+        @if (session('message'))
+            <div class="container alert alert-dark" role="alert">
+                {{ session('message') }}
             </div>
         @endif
 
-        <div class="pt-8 sm:pt-0">
-            <form action="/staff/moh/articles/{{$article->id}}/update" enctype="multipart/form-data" method="POST" class="add-article-form">
-                <h1 class="add-hero2">Update Article</h1>
+        <div class="text-start shadow container bg-white mt-5 rounded px-5 py-3 text-dark">
+            <h4 class="mb-3 text-center"> Update Article # {{$article->id}} </h4>    
+            <form method="POST">
                 @csrf
-                <div style="margin-left: 18rem;margin-top: 1rem;">
-                    <label for="title">Article title</label>
-                    <input style="width: 25rem;" type="text" name="title" id="title" placeholder="Enter article title" value="{{$article->title}}"
-                        required>
+                <div class="row">
+                    <div class="col-12 col-md-6">
+                        <label>Title</label>
+                        <input value="{{$article->title}}" type="text" class="form-control" name="title" placeholder="Enter article title" required>
+                    </div>
+
+                    <div class="col-12 col-md-6">
+                        <label>Add image</label>
+                        <br>
+                        <input class="form-control" type="file" name="image">
+                    </div>
                 </div>
-                <br>
-                <div style="margin-left: 18rem;">
-                    <label for="content">Article content</label>
-                    <br>
-                    <textarea style="margin-left: 7rem;margin-top: -2rem;" name="content" id="content" cols="30" rows="10"
-                        placeholder="Enter article content" required>{{$article->content}}</textarea>
+
+                <div class="row mt-2">
+                    <div class="col-12 col-md-6">
+                        <label>Content</label>
+                        <textarea value="{{$article->content}}" class="form-control" rows="5" name="content" placeholder="Enter article content" required></textarea>
+                    </div>
+
+                    <div class="col-12 col-md-6">
+                        <label>(Optional) Link to the full article</label>
+                        <input value="{{$article->full_link}}" class="form-control" type="text" name="full_link" placeholder="Enter full article link">
+                    
+                        <label class="mt-2">(Optional) Link to video</label>
+                        <input value="{{$article->link}}" class="form-control" type="text" name="link" placeholder="Enter full article link">
+                    </div>
                 </div>
-                <br>
-                <div style="margin-left: 18rem;">
-                    <label for="image">Add image</label>
-                    <input type="file" name="image" id="image" style="margin-left: 1.5rem;">
+
+                <div class="container text-center my-3">
+                    <button type="submit" style="width: 300px;" class="btn btn-success">Update</button>
                 </div>
-                <br>
-                <div style="margin-left: 18rem;">
-                    <label for="full-link">(Optional) Link to the full article</label>
-                    <input type="text" name="full_link" id="full-link" placeholder="Enter full article link"  value="{{$article->full_article_link}}">
-                </div>
-                <br>
-                <div style="margin-left: 18rem;">
-                    <label for="video">(Optional) Link to video</label>
-                    <input type="text" name="link" id="video" placeholder="Enter video link" value="{{$article->video_link}}">
-                </div>
-                <br>
-                <input type="submit" value="Update" class="submit" style="margin-top: -2rem;">
-                <br>
             </form>
 
         </div>
