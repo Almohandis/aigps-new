c21e2a2c9bf2b145b083cd73cc9d0f9438c1ac60
Almohndis <ur843d@gmail.com>
2022-03-29 17:05:17 +0200

    Infection count for each age segment chart added

--- resources/views/statistics/blood-type-dist.blade.php
+++ resources/views/statistics/blood-type-dist.blade.php
@@ -5,16 +5,6 @@
                 {{ session('message') }}
             @endif
         </div>
-        <style>
-            .tr {
-                display: table-cell;
-            }
-
-            .td {
-                display: block;
-            }
-
-        </style>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.1/chart.min.js"
                 integrity="sha512-QSkVNOCYLtj73J4hbmVoOV6KVZuMluZlioC+trLpewV8qMjsWqlIQvkn1KGX2StWvPMdWGBqim1xlC8krl1EKQ=="
                 crossorigin="anonymous" referrerpolicy="no-referrer"></script>
