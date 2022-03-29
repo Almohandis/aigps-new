c555d365a62a4f97cda8ea786f5bf2fc303aec60
Almohndis <ur843d@gmail.com>
2022-03-29 15:05:46 +0200

    Infections with respect to cities chart added

--- resources/views/statistics/infections-report.blade.php
+++ resources/views/statistics/infections-report.blade.php
@@ -5,6 +5,9 @@
                 {{ session('message') }}
             @endif
         </div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.1/chart.min.js"
+                integrity="sha512-QSkVNOCYLtj73J4hbmVoOV6KVZuMluZlioC+trLpewV8qMjsWqlIQvkn1KGX2StWvPMdWGBqim1xlC8krl1EKQ=="
+                crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <div class="pt-8 sm:pt-0">
             <form id="form" action="/stats" method="POST">
                 @csrf
@@ -35,40 +38,53 @@
                 </div>
                 <div class="tbl-content">
                     <table>
-                        @for ($i = 0, $j = 0; $i < count($cities); $i++)
+                        @foreach ($data_by_city as $city)
                             <tr>
-                                <td>{{ $cities[$i] }}</td>
-                                @if (isset($data_by_city[$j]))
-                                    @if ($cities[$i] == $data_by_city[$j]->city)
-                                        <td>{{ $data_by_city[$j]->male_count }}</td>
-                                        <td>{{ $data_by_city[$j]->male_pcnt }}</td>
-                                        <td>{{ $data_by_city[$j]->female_count }}</td>
-                                        <td>{{ $data_by_city[$j]->female_pcnt }}</td>
-                                        <td>{{ $data_by_city[$j]->total_infections }}</td>
-                                        <td>{{ $data_by_city[$j]->tot_hos }}</td>
-                                        <td>{{ $data_by_city[$j]->avg_avail_beds }}</td>
-                                        @php $j++ @endphp
-                                    @else
-                                        <td>0</td>
-                                        <td>0</td>
-                                        <td>0</td>
-                                        <td>0</td>
-                                        <td>0</td>
-                                        <td>0</td>
-                                        <td>0</td>
-                                    @endif
-                                @else
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                @endif
+                                <td>{{ $city->city }}</td>
+                                <td>{{ $city->male_count }}</td>
+                                <td>{{ $city->male_pcnt }}</td>
+                                <td>{{ $city->female_count }}</td>
+                                <td>{{ $city->female_pcnt }}</td>
+                                <td>{{ $city->total_infections }}</td>
+                                <td>{{ $city->tot_hos }}</td>
+                                <td>{{ $city->avg_avail_beds }}</td>
                             </tr>
-                        @endfor
+                        @endforeach
                     </table>
+                </div>
+                <div>
+                    @php
+                        $counter = count($data_by_city);
+                        $sum = 0;
+                        $mean = 0;
+                        $variance = 0;
+                        $standard_deviation = 0;
+
+                        foreach ($data_by_city as $city) {
+                            $sum += $city->total_infections;
+                        }
+
+                        $mean = round($sum / $counter, 2);
+
+                        $total = $sum;
+                        $sum = 0;
+
+                        foreach ($data_by_city as $city) {
+                            $sum += pow($city->total_infections - $mean, 2);
+                        }
+
+                        $variance = round($sum / $counter, 2);
+
+                        $standard_deviation = round(sqrt($variance), 2);
+
+                    @endphp
+                </div>
+                <div>
+                    <P>Total infections = {{ $total }}</p>
+                    <canvas id="infections" width="200" height="100"></canvas>
+                    <p>Infections mean (µ) = {{ $mean }}</p>
+                    <p>Infections variance (σ<sup>2</sup>) = {{ $variance }}</p>
+                    <P>Infections standard deviation (σ) = {{ $standard_deviation }}</P>
                 </div>
             @elseif(isset($data_by_vaccine_status))
                 <h1>{{ $report_title }}</h1>
@@ -160,5 +176,38 @@
             @endif
         </div>
     </div>
+    @if (isset($data_by_city))
+        <script>
+            const canvas = document.getElementById('infections').getContext('2d');
+
+            let xlabels = [];
+
+            @foreach ($data_by_city as $city)
+                xlabels.push('{{ $city->city }}');
+            @endforeach
+
+            let ylabels = [];
+
+            @foreach ($data_by_city as $city)
+                ylabels.push('{{ $city->total_infections }}');
+            @endforeach
+
+            let data = {
+                labels: xlabels,
+                datasets: [{
+                    label: 'Infections count',
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: ylabels
+                }]
+            };
+
+            let config = {
+                type: 'bar',
+                data: data
+            };
+            new Chart(canvas, config);
+        </script>
+    @endif
     <script src="{{ asset('js/statistics.js') }}"></script>
 </x-app-layout>
