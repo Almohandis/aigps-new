3d99c2e0202f4c70f5fdfc368c4094216c9f31f6
Abdullah Hejazi <a.m.a.hejazi@gmail.com>
2022-03-29 14:07:40 +0200

    campaigns and hospitals

--- resources/views/moh/manage-campaigns.blade.php
+++ resources/views/moh/manage-campaigns.blade.php
@@ -1,183 +1,183 @@
 <x-app-layout>
-    <div class="max-w-6xl mx-auto sm:px-6 lg:px-8 mt-9">
-        <div class="notification">
-            @if (session('message'))
+    <link href="{{asset('css/reservation.css')}}" rel="stylesheet">
+
+    <div class="mt-5 text-center">
+        <h1 class="aigps-title">Manage Campaigns</h1>
+
+        @if (session('message'))
+            <div class="container alert alert-dark" role="alert">
                 {{ session('message') }}
-            @endif
-        </div>
-        <div class="pt-8 sm:pt-0">
-            <h1 class="add-hero2">All upcoming campaigns</h1><br>
-            <form action="/staff/moh/manage-campaigns/add" id="procceed_form" method="POST">
-                @csrf
-                <input id="marker-location" type="hidden" name="location" value="">
-                <div class="tbl-header">
-                    <table>
+            </div>
+        @endif
+
+        <div class="table-responsive text-start shadow container bg-white mt-5 rounded px-5 py-3 text-dark">
+            <h4 class="text-center mb-3"> All Campaigns </h4>
+    
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">Campaign's start date</th>
+                        <th scope="col">Campaign's end date</th>
+                        <th scope="col">City</th>
+                        <th scope="col">Address</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Cancel</th>
+                        <th scope="col">Update</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($campaigns as $campaign)
                         <tr>
-                            <th>Campaign's start date</th>
-                            <th>Campaign's end date</th>
-                            <th>City</th>
-                            <th>Address</th>
-                            <th>Status</th>
-                            <th>Cancel</th>
-                            <th>Update</th>
+                            <td>{{ $campaign->start_date }}</td>
+                            <td>{{ $campaign->end_date }}</td>
+                            <td>{{ $campaign->city }}</td>
+                            <td>{{ $campaign->address }}</td>
+                            <td>{{ $campaign->status }}</td>
+                            <td>
+                                <a class="btn btn-outline-danger" href="/staff/moh/manage-campaigns/{{ $campaign->id }}/delete"> Delete </a>
+                            </td>
+
+                            <td>
+                                <a class="btn btn-outline-primary" href="/staff/moh/manage-campaigns/{{ $campaign->id }}/update"> Update </a>
+                            </td>
                         </tr>
-                    </table>
-                </div>
-                <div class="tbl-content">
-                    <table>
-                        @foreach ($campaigns as $campaign)
-                            <tr>
-                                <td>{{ $campaign->start_date }}</td>
-                                <td>{{ $campaign->end_date }}</td>
-                                <td>{{ $campaign->city }}</td>
-                                <td>{{ $campaign->address }}</td>
-                                <td>{{ $campaign->status }}</td>
-                                <td><a class="text-red-500"
-                                        href="/staff/moh/manage-campaigns/{{ $campaign->id }}/delete"> Delete </a>
-                                </td>
-
-                                <td><a class="text-blue-500"
-                                        href="/staff/moh/manage-campaigns/{{ $campaign->id }}/update"> Update </a>
-                                </td>
-                            </tr>
-                        </form>
                     @endforeach
-                </table>
-            </div>
-            
-            <!-- pagination -->
-            <div class="pagination">
-                {{ $campaigns->links() }}
-            </div>
+                </tbody>
+            </table>
 
-            <br>
-            <form action="/staff/moh/manage-campaigns/add" id="procceed_form" method="POST">
+            <div class="flex">
+                <ul class="pagination justify-content-center">
+                    @if ($campaigns->previousPageUrl())
+                        <li class="page-item"><a class="page-link" href="/staff/moh/manage-campaigns?page={{ $campaigns->currentPage() - 1 }}">Previous</a></li>
+                    @endif
+                    
+                    
+                    @for($page = 1; $page <= $campaigns->lastPage(); $page++)
+                        <li class="page-item"><a class="page-link" href="/staff/moh/manage-campaigns?page={{ $page }}">{{ $page }}</a></li>
+                    @endfor
+
+                    @if ($campaigns->nextPageUrl())
+                        <li class="page-item"><a class="page-link" href="/staff/moh/manage-campaigns?page={{ $campaigns->currentPage() + 1 }}">Next</a></li>
+                    @endif
+                </ul>
+            </div>
+        </div>
+
+        <div class="text-start shadow container bg-white mt-5 rounded px-5 py-3 text-dark">
+            <h4 class="mb-3 text-center"> Add a new campaign </h4>    
+            <form action="/staff/moh/manage-campaigns/add" method="POST">
                 @csrf
                 <input id="marker-location" type="hidden" name="location" value="">
-                <div class="add-campaign">
-                    <h2 class="add-hero">Add new campaign</h2>
-                    <div class="mt-4">
-                        <label for="start_date" style="margin-left: 3rem;">Start date</label>
-                        <input type="date" name="start_date" style="border-color: gray;border-width: 1px;" required>
-                        <label for="end_date" style="margin-left: 16rem;">End date</label>
-                        <input type="date" name="end_date" style="border-color: gray;border-width: 1px;" required><br>
-                        <div style="margin-left: 3rem;margin-top: 2rem;">
-                            <label for="capacity">Capacity per day</label>
-                            <input type="number" min="1" name="capacity_per_day" id="capacity"
-                                style="border-color: gray;border-width: 1px;"><br>
-                        </div>
-                        <div style="margin-left: 3rem;margin-top: 2rem;">
-                            <label for="city">City</label>
-                            <select name="city" class="block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" style="width: 18rem;margin-left: 3rem;margin-top: -2rem;">
-                                @foreach ($cities as $city)
-                                    <option value="{{ $city }}">{{ $city }}</option>
-                                @endforeach
-                            </select>
-                            <br>
-                        </div>
-                        <div style="  margin-top: -3.5rem;margin-left: 20rem;">
-                            <label id="addressLabel" for="address" style="margin-left: 6rem;">Address</label>
-                            <input type="text" name="address" id="address" style="margin-left: 0rem;height: 2.3rem;"
-                                required><input type="button" id="search-button" value="Search"
-                                class="update-btn5"><br>
-                        </div>
-                        <div style="margin-top: 2rem;margin-left: 3rem;">
-                            <label for="Doctors">Doctors</label>
-                            <input type="button" id="doctor-add-button" value="Add doctor" class="add-doc-btn">
-                        </div>
+                <div class="form-group row">
+                    <div class="col-6">
+                        <label>Start Date</label>
+                        <input type="date" class="form-control" name="start_date" required>
+                    </div>
+
+                    <div class="col-6">
+                        <label>End Date</label>
+                        <input type="date" class="form-control" name="end_date" required>
                     </div>
                 </div>
-            </div>
-    </div>
-    </form>
-    <h3 class="add-hero">Choose a location on the map</h3>
-    {{-- !! --}}
-    <div class="mx-auto text-center mt-5">
-        <div id="map" class="mt-8 rounded-md border-solid border-4 border-black"
-            style="width: 119%; height: 600px; max-height: 90vh; margin: 0px auto; position: relative; overflow: hidden;margin-left: -6rem;""></div>
-                        <script src="https://maps.googleapis.com/maps/api/js?key={{ config('app.google_maps_api') }}&callback=initMap" defer>
-                        </script>
-                        <script>
-                            let marker;
-                            let geocoder;
-                            let map;
 
-                            document.getElementById('search-button').addEventListener('click', () => {
-                                geocode({
-                                    address: document.getElementById('address').value
-                                });
+                <div class="form-group row">
+                    <div class="col-6">
+                        <label>Capacity</label>
+                        <input class="form-control" type="number" min="1" name="capacity_per_day" required>
+                    </div>
+
+                    <div class="col-6">
+                        <label>City</label>
+                        <select name="city" class="form-control">
+                            @foreach ($cities as $city)
+                                <option value="{{ $city }}">{{ $city }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <div class="col">
+                        <label>Address</label>
+                        <input class="form-control" type="text" name="address" required>
+                    </div>
+                </div>
+
+                <div class="mt-3">
+                    <h5>Select the location of the campaign in the map</h5>
+                    <div id="map" class="aigps-map"></div>
+                    <script src="https://maps.googleapis.com/maps/api/js?key={{ config('app.google_maps_api') }}&callback=initMap" defer></script>
+                    <script>
+                        let marker;
+                        let geocoder;
+                        let map;
+
+                        function initMap() {
+
+                            map = new google.maps.Map(document.getElementById('map'), {
+                                mapId: "dcba2c77acce5e73",
+                                zoom: 6,
+                                center: new google.maps.LatLng(26.8206, 30.8025),
+                                mapTypeControl: false,
                             });
 
-                            function initMap() {
+                            var infowindow = new google.maps.InfoWindow();
+                            let markerIcon = {
+                                path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+                                fillColor: "black",
+                                fillOpacity: 0.7,
+                                strokeWeight: 0,
+                                rotation: 0,
+                                scale: 2,
+                                anchor: new google.maps.Point(15, 30),
+                            };
 
-                                map = new google.maps.Map(document.getElementById('map'), {
-                                    mapId: "dcba2c77acce5e73",
-                                    zoom: 6,
-                                    center: new google.maps.LatLng(26.8206, 30.8025),
-                                    mapTypeControl: false,
+                            geocoder = new google.maps.Geocoder();
+
+                            marker = new google.maps.Marker({
+                                position: map.getCenter(),
+                                map: map,
+                                icon: markerIcon,
+                                draggable: true,
+                            });
+
+                            map.addListener('center_changed', () => {
+                                marker.setPosition(map.getCenter());
+                                document.getElementById('marker-location').value = marker.getPosition();
+                            });
+
+                            marker.addListener('dragend', () => {
+                                document.getElementById('marker-location').value = marker.getPosition();
+                            });
+
+                        }
+
+                        function geocode(request) {
+                            marker.setMap(null);
+                            geocoder
+                                .geocode(request)
+                                .then((result) => {
+                                    const {
+                                        results
+                                    } = result;
+                                    map.zoom = 16;
+                                    map.setCenter(results[0].geometry.location);
+                                    marker.setPosition(results[0].geometry.location);
+                                    marker.setMap(map);
+                                    return results;
+                                })
+                                .catch((e) => {
+                                    alert("Couldn't find the location, please search with another address");
                                 });
+                        }
+                    </script>
+                </div>
 
-                                var infowindow = new google.maps.InfoWindow();
-                                let markerIcon = {
-                                    path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
-                                    fillColor: "black",
-                                    fillOpacity: 0.7,
-                                    strokeWeight: 0,
-                                    rotation: 0,
-                                    scale: 2,
-                                    anchor: new google.maps.Point(15, 30),
-                                };
+                <div class="container text-center my-3">
+                    <button type="submit" style="width: 300px;" class="btn btn-success">Add</button>
+                </div>
+            </form>
 
-                                geocoder = new google.maps.Geocoder();
-
-                                marker = new google.maps.Marker({
-                                    position: map.getCenter(),
-                                    map: map,
-                                    icon: markerIcon,
-                                    draggable: true,
-                                });
-
-                                map.addListener('center_changed', () => {
-                                    marker.setPosition(map.getCenter());
-                                    document.getElementById('marker-location').value = marker.getPosition();
-                                });
-
-                                marker.addListener('dragend', () => {
-                                    document.getElementById('marker-location').value = marker.getPosition();
-                                });
-
-                            }
-
-                            function geocode(request) {
-                                marker.setMap(null);
-                                geocoder
-                                    .geocode(request)
-                                    .then((result) => {
-                                        const {
-                                            results
-                                        } = result;
-                                        map.zoom = 16;
-                                        map.setCenter(results[0].geometry.location);
-                                        marker.setPosition(results[0].geometry.location);
-                                        marker.setMap(map);
-                                        return results;
-                                    })
-                                    .catch((e) => {
-                                        alert("Couldn't find the location, please search with another address");
-                                    });
-                            }
-                        </script>
-                    </div>
-
-                    <div class="                   mt-6" style="margin-right: -8rem;">
-            <div class="mt-3 mx-auto text-right mr-5">
-                <x-button type="submit" id="procceed_button" style="margin-bottom: 1rem;">
-                    Procceed
-                </x-button>
-            </div>
         </div>
-        {{-- !! --}}
     </div>
-    </div>
-    <script src="{{ asset('js/manage-campaigns.js') }}"></script>
 </x-app-layout>
--- resources/views/moh/manage-hospitals.blade.php
+++ resources/views/moh/manage-hospitals.blade.php
@@ -1,80 +1,101 @@
 <x-app-layout>
-    <div class="max-w-6xl mx-auto sm:px-6 lg:px-8 mt-9">
-        <div class="notification">
-            @if (session('message'))
-                {{ session('message') }}
-            @endif
-        </div>
-        <div class="pt-8 sm:pt-0">
+    <link href="{{asset('css/reservation.css')}}" rel="stylesheet">
 
-            <div class="add-hospital">
-                <h1 class="add-hero">Add new hospital</h1>
-                <form action="/staff/moh/manage-hospitals/add" method="POST">
-                    @csrf
-                    <div style="margin-top: 2rem;margin-left: 4rem;">
-                        <label for="hospital">Hospital name</label>
-                        <input required type="text" name="name">
+    <div class="mt-5 text-center">
+        <h1 class="aigps-title">Manage Hospitals</h1>
+
+        @if (session('message'))
+            <div class="container alert alert-dark" role="alert">
+                {{ session('message') }}
+            </div>
+        @endif
+
+        <div class="table-responsive text-start shadow container bg-white mt-5 rounded px-5 py-3 text-dark">
+            <h4 class="text-center mb-3"> All Hospitals </h4>
+    
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">City</th>
+                        <th scope="col">Total capacity</th>
+                        <th scope="col">Update</th>
+                        <th scope="col">Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($hospitals as $hospital)
+                        <tr>
+                            <td>{{ $hospital->name }}</td>
+                            <td>{{ $hospital->city }}</td>
+                            <td>{{ $hospital->capacity }}</td>
+                            <td>
+                                <a class="btn btn-outline-primary" href="/staff/moh/manage-hospitals/{{$hospital->id}}/update"> Update </a>
+                            </td>
+                            <td>
+                                <a class="btn btn-outline-danger" href="/staff/moh/manage-hospitals/{{$hospital->id}}/delete"> Delete </a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+
+            <div class="flex">
+                <ul class="pagination justify-content-center">
+                    @if ($hospitals->previousPageUrl())
+                        <li class="page-item"><a class="page-link" href="/staff/moh/manage-hospitals/?page={{ $hospitals->currentPage() - 1 }}">Previous</a></li>
+                    @endif
+                    
+                    
+                    @for($page = 1; $page <= $hospitals->lastPage(); $page++)
+                        <li class="page-item"><a class="page-link" href="/staff/moh/manage-hospitals/?page={{ $page }}">{{ $page }}</a></li>
+                    @endfor
+
+                    @if ($hospitals->nextPageUrl())
+                        <li class="page-item"><a class="page-link" href="/staff/moh/manage-hospitals/?page={{ $hospitals->currentPage() + 1 }}">Next</a></li>
+                    @endif
+                </ul>
+            </div>
+        </div>
+
+        <div class="text-start shadow container bg-white mt-5 rounded px-5 py-3 text-dark">
+            <h4 class="mb-3 text-center"> Add a new Hospital </h4>    
+            <form action="/staff/moh/manage-hospitals/add" method="POST">
+                @csrf
+                <div class="form-group row">
+                    <div class="col-6">
+                        <label>Name</label>
+                        <input type="text" class="form-control" name="name" required>
                     </div>
-                    <div style="margin-left: 42rem;  margin-top: -2.5rem;">
-                        <label for="city">City</label>
-                        <select required name="city" id="city">
-                            <option value="" hidden selected disabled>Select city</option>
+
+                    <div class="col-6">
+                        <label>Is Isolation</label>
+                        <br>
+                        <input class="form-check-input" type="checkbox" name="is_isolation" checked>
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <div class="col-6">
+                        <label>Capacity</label>
+                        <input class="form-control" type="number" min="1" name="capacity" required>
+                    </div>
+
+                    <div class="col-6">
+                        <label>City</label>
+                        <select name="city" class="form-control">
                             @foreach ($cities as $city)
                                 <option value="{{ $city }}">{{ $city }}</option>
                             @endforeach
                         </select>
-                    </div><br>
-                    <label for="capacity" style="margin-left: 6.5rem;">Capacity</label>
-                    <input required type="number" name="capacity"
-                        style="border-width: 2px;width: 14rem;height: 2rem;"><br>
-                    <input type="checkbox" name="is_isolation" style="margin-left: 43rem;margin-top: -3rem;">
-                    <div style="margin-left: 45rem;margin-top: -3rem;">Isolation</div><br>
+                    </div>
+                </div>
 
-                    <input type="submit" value="Add hospital" class="update-btn3">
+                <div class="container text-center my-3">
+                    <button type="submit" style="width: 300px;" class="btn btn-success">Add</button>
+                </div>
+            </form>
 
-                </form>
-            </div>
-
-            <h1 class="add-hero">All hospitals</h1>
-            
-            <div class="tbl-header">
-                <table>
-                    <tr>
-                        <th>Name</th>
-                        <th>City</th>
-                        <th>Total capacity</th>
-                        {{-- <td>Intensive care beds</td>
-                <td>Available intensive care beds</td>
-                <td>Available regular beds</td> --}}
-                        <th>Update</th>
-                        <th>Delete</th>
-                    </tr>
-
-                </table>
-            </div>
-            <div class="tbl-content">
-                <table>
-                    @foreach ($hospitals as $hospital)
-                        <tr>
-                            <input type="hidden" class="id" value="{{ $hospital->id }}" name="id[]">
-                            <td>{{ $hospital->name }}</td>
-                            <td>{{ $hospital->city }}</td>
-                            <td>{{ $hospital->capacity }}</td>
-                            <td><a href="/staff/moh/manage-hospitals/{{$hospital->id}}/update" class="text-blue-500"> Update </a></td>
-                            <td><a href="/staff/moh/manage-hospitals/{{$hospital->id}}/delete" class="text-red-500"> Delete </a></td>
-                            
-                        </tr>
-                    @endforeach
-                </table>
-            </div>
-
-            <!-- pagination -->
-            <div class="pagination">
-                {{ $hospitals->links() }}
-            </div>
-            
-            
         </div>
     </div>
-    <script src="{{ asset('js/manage-hospitals.js') }}"></script>
 </x-app-layout>
