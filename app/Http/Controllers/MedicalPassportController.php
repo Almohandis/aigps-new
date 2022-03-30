3cfa5eb961a2a8bb6ff70246b577acc574ab83d7
Abdullah Hejazi <a.m.a.hejazi@gmail.com>
2022-03-30 18:49:14 +0200

    profile

--- app/Http/Controllers/MedicalPassportController.php
+++ app/Http/Controllers/MedicalPassportController.php
@@ -3,24 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\MedicalPassport;
 
-class MedicalPassportController extends Controller
-{
-    public function index(Request $request)
-    {
-        if (!$request->passport_number) {
-            return redirect()->back()->with('message', 'Please enter you passport number'); //'Не введен номер паспорта');
-        }
-        if (!$request->user()->passport()->first()) {
-            return redirect()->back()->with('message', 'You have no passport yet');
-        }
-        $request->user()->passport()->first()->update([
-            'passport_number' => $request->passport_number,
+class MedicalPassportController extends Controller {
+    public function index(Request $request) {
+        $request->validate([
+            'passport_number'       =>      ['required', 'min:8']
         ]);
-        $medical_passport = $request->user()->passport()->first();
-        $vaccine_dates = $request->user()->passport()->first()->dates()->get();
-        $user = $request->user();
-        $date = now();
-        return view('medical-passport', compact('medical_passport', 'vaccine_dates', 'user', 'date'));
+
+        $passport = $request->user()->passport()->first();
+
+        if (! $passport) {
+            return back()->withErrors('You have no medical passport !');
+        }
+
+        $passport->passport_number = $request->passport_number;
+        $passport->save();
+
+        $dates = $passport->dates()->get();
+
+        return view('medical-passport')
+            ->with('passport', $passport)
+            ->with('date', now())
+            ->with('dates', $dates);
     }
 }
