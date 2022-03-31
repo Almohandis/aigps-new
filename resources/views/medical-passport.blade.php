4027aef03f50fd3f46615ef50d8b66b6efdab1e3
Abdullah Hejazi <a.m.a.hejazi@gmail.com>
2022-03-31 09:39:59 +0200

    print medical passport pdf

--- resources/views/medical-passport.blade.php
+++ resources/views/medical-passport.blade.php
@@ -5,6 +5,7 @@
         <div class="text-start shadow container bg-white mt-5 rounded p-5 text-dark">
             <h3 class="text-center mb-3">VACCINE CERTIFICATE</h3>
 
+            <div id="medicalPassportTable">
             <table class="table table-striped">
                 <tbody>
                     <tr>
@@ -55,7 +56,37 @@
                     </tr>
                 </tbody>
             </table>
+</div>
+
+            <div class="row justify-content-center">
+                <button class="btn btn-primary" onclick="createPDF()">Print</button>
+            </div>
             
+            <script>
+                function createPDF() {
+                    var sTable = document.getElementById('medicalPassportTable').innerHTML;
+
+                    var style = "<style>";
+                    style = style + "table {width: 100%;font: 17px Calibri;}";
+                    style = style + "table, th, td {border: solid 1px #DDD; border-collapse: collapse;";
+                    style = style + "padding: 2px 3px;text-align: center;}";
+                    style = style + "</style>";
+
+                    var win = window.open('', '', 'height=700,width=700');
+
+                    win.document.write('<html><head>');
+                    win.document.write('<title>Profile</title>');
+                    win.document.write(style);
+                    win.document.write('</head>');
+                    win.document.write('<body>');
+                    win.document.write(sTable);
+                    win.document.write('</body></html>');
+
+                    win.document.close();
+
+                    win.print();
+                }
+            </script>
         </div>
     </div>
 </x-app-layout>
